245120d389cafc86fdb5b7d478caf935d7271b79
mateusz <cziken58@gmail.com>
2013-08-03 14:43:55 +0200

    Odświeżone zasoby

--- mobs/pack.sh
+++ mobs/pack.sh
@@ -1,3 +1,3 @@
 rm filesystem.vfs
-./archiver filesystem.vfs mateusz bambus1.txt bron.txt chmura1.txt chmura2.txt drzewo1.txt cpp.txt drzewo2.txt gracz.txt kaktus.txt kwiat1.txt kwiat2.txt mapa.txt menu.txt paralax_1.txt pocisk.txt pocisk_zielony.txt punkt.txt stalagmit1.txt trawa.txt trawa1.txt ufo.txt wrog.txt zycie.txt kolce.txt
+./archiver filesystem.vfs mateusz bron.txt czaszka.txt gracz.txt kolce.txt kupa.txt mapa.txt menu.txt paralax_1.txt pocisk.txt pocisk_zielony.txt wrog.txt zycie.txt punkt.txt shadow_frag_shader.txt hit_frag_shader.txt 
 cp filesystem.vfs ../filesystem.vfs
